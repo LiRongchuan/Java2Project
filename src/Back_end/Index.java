e5c2c1a0180976f1ca07b757aa38300f4f24b7e6
hatfail <1833943280@qq.com>
2024-05-30 22:14:25 +0800

    更改

--- src/Back_end/Index.java
+++ src/Back_end/Index.java
@@ -6,24 +6,32 @@ import java.util.Scanner;
 
 public class Index {
     private ArrayList<String> index_wordbook = new ArrayList<>();
-
-    public void setIndex_wordbook(String new_wordbook) {
+    private final String FileIndex = "";
+    public void setIndex_wordbook(String new_wordbook) throws IOException {
         this.index_wordbook.add(new_wordbook);
+        PrintIndex();
+    }
+
+    public void PrintIndex() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(FileIndex))));
+        for (String print: index_wordbook) {
+            bw.write(print+"\n");
+        }
+        bw.flush();
     }
 
     public ArrayList<String> getIndex_wordbook() {
         return index_wordbook;
     }
 
-    public void ScanIndex() {
+    public void LoadIndex() throws IOException {
         String index = "";
-        File file = new File(index);
+        File file = new File(FileIndex);
         Scanner scanner;
-        try {
-            scanner = new Scanner(file);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        if (file.exists()) {
+            file.createNewFile();
         }
+        scanner = new Scanner(file);
         while (scanner.hasNext()) {
             index = scanner.nextLine();
             index_wordbook.add(index);
