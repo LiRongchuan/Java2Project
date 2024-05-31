4c0a303aaf6c5c1d90f20010a8c65febefc2b31b
Scott-Einstein <34189542+Scott-Einstein@users.noreply.github.com>
2024-05-31 19:31:49 +0800

    update

--- src/Back_end/WordBook.java
+++ src/Back_end/WordBook.java
@@ -89,32 +89,33 @@ public class WordBook {
     }
 
     public void Generate_Word_Groups() {
-        int grp=1,tot=0,ingroup=0;
+        int grp = 1, tot = 0, ingroup = 0;
         WordGroup wg = new WordGroup();
         int[] num = new int[10];
-        while (tot<words.size()) {
+        while (tot < words.size()) {
             Word wd = words.get(tot);
-            num[ingroup]=wd.getId();
+            num[ingroup] = wd.getId();
             tot++;
             ingroup++;
-            if (ingroup==10) {
+            if (ingroup == 10) {
                 wg.setContain(num);
                 wg.setGroupNum(grp);
                 wordGroups.add(wg);
                 wg = new WordGroup();
                 grp++;
-                ingroup=0;
+                ingroup = 0;
             }
         }
-        for (int i=ingroup;i<10;i++) num[i]=0;
+        for (int i = ingroup; i < 10; i++) num[i] = 0;
         wg.setContain(num);
         wg.setGroupNum(grp);
         wordGroups.add(wg);
     }
+
     public void saveWordsToFile() throws IOException {
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(this.fileLocation)));
         writer.write("WordList:\n");
-        for (Word word: words) {
+        for (Word word : words) {
             writer.write(word.toString());
         }
         writer.flush();
@@ -133,13 +134,14 @@ public class WordBook {
         wg.sort(new Comparator<WordGroup>() {
             @Override
             public int compare(WordGroup o1, WordGroup o2) {
-                double E1=o1.getCurve().getE(),E2=o2.getCurve().getE();
-                if (E1<=E2) return 1;
+                double E1 = o1.getCurve().getE(), E2 = o2.getCurve().getE();
+                if (E1 <= E2) return 1;
                 return -1;
             }
         });
         return wg.get(0);
     }
+
     public File getFileLocation() {
         return fileLocation;
     }
--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,10 @@ import java.util.ArrayList;
 public class Main {
     static Timestamp systemTime; // 模拟系统时间，人为跳转时间，方便演示
     static ArrayList<WordBook> WordBooks = new ArrayList<>();
-    static WordList wordList = new WordList();
+
+    static File dictionary;
+    // 词典，一个存所有单词的文件，用WordList类管理
+    static WordList wordList = new WordList(dictionary);
     public static void main(String[] args) throws IOException {
         Index index = new Index();
         index.LoadIndex();
