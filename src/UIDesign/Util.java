5ffd06b53af22b536af2fc361cb74aa8533f02d8
alkane7 <133130114+alkane7@users.noreply.github.com>
2024-06-01 21:09:03 +0800

    Update Util.java

--- src/UIDesign/Util.java
+++ src/UIDesign/Util.java
@@ -1,5 +1,7 @@
 package UIDesign;
 
+import java.awt.*;
+
 public class Util {
     public static final int FRAME_WIDTH = 800;
     public static final int FRAME_HEIGHT = 600;
@@ -7,4 +9,9 @@ public class Util {
     public static final int Y0 = 0;
     public static final int pageMarginWidth = 100, pageMarginHeight = 150, pageSpace = 250;
     public static final int pageButtonSize = 150;
+    public static final Color pink1 = new Color(255, 192, 192, 118);
+    public static final Color wine = new Color(133, 6, 6, 255);
+    public static final Color pinkbackground = new Color(244, 196, 204, 255);
+    public static final Color c_green = new Color(112, 180, 136, 255);
+    public static final Color w_red = new Color(234, 37, 18, 255);
 }
