b23560a8ebf03bc88f67efa9660efcb2f5e984d9
alkane7 <133130114+alkane7@users.noreply.github.com>
2024-06-02 18:15:05 +0800

    Update Frame.java

--- src/UIDesign/Frame.java
+++ src/UIDesign/Frame.java
@@ -16,11 +16,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.sql.Date;
 import java.util.Vector;
@@ -35,8 +32,12 @@ public class Frame extends JFrame {
     public static Reciteword reciteword;
 
     public static StarPage starPage;
-    public static ArrayList<String> recordname;
-    public static ArrayList<Double> recordrate;
+    public static ArrayList<String> recordname = new ArrayList<>();
+    public static ArrayList<Double> recordrate = new ArrayList<>();
+    //词书名称 cnt 来控制哪些天背什么
+    public static ArrayList<String[]> routine = new ArrayList<>();
+
+//    public static ArrayList<String[]> deterFile = new ArrayList<>();
 
     public void launchFrame() throws IOException {
         this.setLayout(null);
@@ -83,39 +84,76 @@ public class Frame extends JFrame {
     }
 }
 class HomePage extends JPanel {
+    private Font youyuan = new Font("幼圆",Font.BOLD,16);
     JButton learn, setting, graph;
     JPanel pagePanel, bookPanel;
+    JTextField learnText, settingText, graphText;
     int bookMarginWidth = 300, bookMarginHeight = 100;
     int bookSpaceHorizontal = 450, bookSpaceVertical = 400;
     int bookButtonWidth = 200, bookButtonHeight = 300;
     public void launchPanel(){
         this.setLayout(null);
         this.setBounds(0,0,FRAME_WIDTH,FRAME_HEIGHT);
-        pagePanel = new JPanel();
+//        this.setBackground(Color.white);
+        pagePanel = new ImagePanel(new ImageIcon("picture\\panel1.jpg").getImage());
         pagePanel.setBounds(0, 0, 170, FRAME_HEIGHT);
-        pagePanel.setBackground(Color.PINK);
         pagePanel.setLayout(null);
-        learn = new JButton("背单词");
+        learn = new JButton();
+        learn.setIcon(SwingUtil.createAutoAdjustIcon(new ImageIcon("picture\\单词本1.png").getImage(), true));
         learn.setBounds(35, 40, 100, 100);
+        learn.setBorderPainted(true);
+        learn.setContentAreaFilled(false);
         learn.addActionListener(e -> {
             Frame.homePage.setVisible(false);
             Frame.reciteword.setVisible(true);
         });
-        setting = new JButton("我的");
+        learnText = new JTextField("背单词");
+        learnText.setBounds(60, 140, 55, 20);
+        learnText.setOpaque(false);
+        learnText.setEditable(false);
+        learnText.setBorder(null);
+        learnText.setFont(youyuan);
+
+//        setting = new JButton("我的");
+        setting = new JButton("");
+        setting.setIcon(SwingUtil.createAutoAdjustIcon(new ImageIcon("picture\\我的.png").getImage(), true));
         setting.setBounds(35, 220, 100, 100);
+        setting.setBorderPainted(true);
+        setting.setContentAreaFilled(false);
         setting.addActionListener(e -> {
             Frame.homePage.setVisible(false);
             Frame.settingPage.setVisible(true);
         });
-        graph = new JButton("记忆曲线");
+        settingText = new JTextField("我的");
+        settingText.setBounds(65, 320, 55, 20);
+        settingText.setOpaque(false);
+        settingText.setEditable(false);
+        settingText.setBorder(null);
+        settingText.setFont(youyuan);
+
+//        graph = new JButton("记忆曲线");
+        graph = new JButton();
+        graph.setIcon(SwingUtil.createAutoAdjustIcon(new ImageIcon("picture\\动态曲线.png").getImage(), true));
         graph.setBounds(35, 400, 100, 100);
+        graph.setBorderPainted(true);
+        graph.setContentAreaFilled(false);
         graph.addActionListener(e -> {
             Frame.homePage.setVisible(false);
             Frame.chartPage.setVisible(true);
         });
+        graphText = new JTextField("记忆曲线");
+        graphText.setBounds(55, 500, 80, 20);
+        graphText.setOpaque(false);
+        graphText.setEditable(false);
+        graphText.setBorder(null);
+        graphText.setFont(youyuan);
+
         pagePanel.add(learn);
         pagePanel.add(setting);
         pagePanel.add(graph);
+        pagePanel.add(learnText);
+        pagePanel.add(settingText);
+        pagePanel.add(graphText);
         this.add(pagePanel);
 
 
@@ -124,14 +162,51 @@ class HomePage extends JPanel {
 //        bookPanel.setBackground(Color.CYAN);
         //bookPanel.setLayout(new FlowLayout()); // 使用FlowLayout作为示例，可以根据需要更改布局管理器
         bookPanel.setLayout(null);
-        JButton book1 = new JButton("词书1");
+        JButton book1 = new JButton();
+        book1.setIcon(SwingUtil.createAutoAdjustIcon(new ImageIcon("picture\\词书图.png").getImage(), true));
         book1.setBounds(30, 50, 200, 150);
-        JButton book2 = new JButton("词书2");
+        book1.setBorderPainted(false);
+        book1.setContentAreaFilled(false);
+        JTextField book1Text = new JTextField("词书一");
+        book1Text.setBounds(110, 170, 80, 50);
+        book1Text.setOpaque(false);
+        book1Text.setEditable(false);
+        book1Text.setBorder(null);
+        book1Text.setFont(youyuan);
+        JButton book2 = new JButton();
+        book2.setIcon(SwingUtil.createAutoAdjustIcon(new ImageIcon("picture\\词书图.png").getImage(), true));
         book2.setBounds(340, 50, 200, 150);
-        JButton book3 = new JButton("词书3");
+        book2.setBorderPainted(false);
+        book2.setContentAreaFilled(false);
+        JTextField book2Text = new JTextField("词书二");
+        book2Text.setBounds(420, 170, 80, 50);
+        book2Text.setOpaque(false);
+        book2Text.setEditable(false);
+        book2Text.setBorder(null);
+        book2Text.setFont(youyuan);
+        JButton book3 = new JButton();
+        book3.setIcon(SwingUtil.createAutoAdjustIcon(new ImageIcon("picture\\词书图.png").getImage(), true));
         book3.setBounds(30, 320, 200, 150);
-        JButton setBook = new JButton("修改词书");
+        book3.setBorderPainted(false);
+        book3.setContentAreaFilled(false);
+        JTextField book3Text = new JTextField("词书三");
+        book3Text.setBounds(110, 440, 80, 50);
+        book3Text.setOpaque(false);
+        book3Text.setEditable(false);
+        book3Text.setBorder(null);
+        book3Text.setFont(youyuan);
+        JButton setBook = new JButton();
+        setBook.setIcon(SwingUtil.createAutoAdjustIcon(new ImageIcon("picture\\词书图.png").getImage(), true));
         setBook.setBounds(340, 320, 200, 150);
+        setBook.setBorderPainted(false);
+        setBook.setContentAreaFilled(false);
+        JTextField setBookText = new JTextField("添加词书");
+        setBookText.setBounds(420, 440, 80, 50);
+        setBookText.setOpaque(false);
+        setBookText.setEditable(false);
+        setBookText.setBorder(null);
+        setBookText.setFont(youyuan);
+
         setBook.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -145,8 +220,7 @@ class HomePage extends JPanel {
                     System.out.println("File to import: " + selectBook.getAbsolutePath());
                     File newBook = new File("wordbook\\" + selectBook.getName());
                     try {
-                        newBook.delete();
-                        Files.copy(selectBook.toPath(), newBook.toPath());
+                        newBook.createNewFile();
                     } catch (IOException ex) {
                         throw new RuntimeException(ex);
                     }
@@ -157,6 +231,10 @@ class HomePage extends JPanel {
         bookPanel.add(book2);
         bookPanel.add(book3);
         bookPanel.add(setBook);
+        bookPanel.add(book1Text);
+        bookPanel.add(book2Text);
+        bookPanel.add(book3Text);
+        bookPanel.add(setBookText);
         this.add(bookPanel);
 
         this.setVisible(true);
@@ -168,23 +246,34 @@ class SettingPage extends JPanel {
     JLabel head, username, userInfo;
     JCalendar calendar;
     User user;
+    private Font youyuan = new Font("幼圆",Font.BOLD,16);
     public void launchPanel(){
         this.setLayout(null);
         this.setBounds(0,0,FRAME_WIDTH,FRAME_HEIGHT);
         this.setVisible(false);
-        pagePanel = new JPanel();
+        pagePanel = new ImagePanel(new ImageIcon("picture\\背景图.jpg").getImage());
         pagePanel.setBounds(0, 0, 170, FRAME_HEIGHT);
-        pagePanel.setBackground(Color.PINK);
         pagePanel.setLayout(null);
         back = new JButton("返回");
+        back.setIcon(SwingUtil.createAutoAdjustIcon(new ImageIcon("picture\\返回箭头.png").getImage(), true));
+        back.setBorderPainted(true);
+        back.setContentAreaFilled(false);
         back.setBounds(35, 40, 100, 100);
         back.addActionListener(e -> {
             Frame.settingPage.setVisible(false);
             Frame.homePage.setVisible(true);
         });
-        information = new JButton("资料");
+//        information = new JButton("资料");
+        information = new JButton("");
+        information.setIcon(SwingUtil.createAutoAdjustIcon(new ImageIcon("picture\\单词列表.png").getImage(), true));
         information.setBounds(35, 220, 100, 100);
+        information.setBorderPainted(false);
+        information.setContentAreaFilled(false);
+
         star = new JButton("收藏单词");
+        star.setIcon(SwingUtil.createAutoAdjustIcon(new ImageIcon("picture\\单词收藏.png").getImage(), true));
+        star.setBorderPainted(true);
+        star.setContentAreaFilled(false);
         star.setBounds(35, 400, 100, 100);
         star.addActionListener(e -> {
             Frame.settingPage.setVisible(false);
@@ -204,25 +293,31 @@ class SettingPage extends JPanel {
             throw new RuntimeException(e);
         }
 
+        Color rim = new Color(229,152,148);
+        Color background = new Color(252,232,230);
         //头像
         head = new JLabel();
         head.setBounds(30, 50, 130, 160);
+        head.setFont(youyuan);
         head.setText("头像");
-        head.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        head.setBorder(BorderFactory.createLineBorder(rim, 2));
         informationPanel.add(head);
         //用户名
         username = new JLabel();
         username.setBounds(60, 200, 100, 100);
+        username.setFont(youyuan);
         username.setText(user.getNickname());
         informationPanel.add(username);
         //信息
         userInfo = new JLabel();
         userInfo.setBounds(15, 300, 160, 200);
-
+        userInfo.setBorder(BorderFactory.createBevelBorder(1,rim, background));
+        userInfo.setBackground(background);
+        userInfo.setOpaque(true);
+        userInfo.setFont(youyuan);
         userInfo.setText("<html>   手机   "+user.getPhone_number()+"<br/><br/><br/>"+
-                        "   邮箱   "+user.getEmail()+"<br/><br/><br/>"+
-                        "   学习目标 "+user.getGoal()+"</html>");
-        userInfo.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+                "   邮箱   "+user.getEmail()+"<br/><br/><br/>"+
+                "   学习目标 "+user.getGoal()+"</html>");
         informationPanel.add(userInfo);
 
         //日历  ///////////////////////////替换为我写的日历
@@ -365,63 +460,90 @@ class ChartPage extends JPanel {
 }
 class StarPage extends JPanel {
     JButton back, information, star;
-    JPanel pagePanel;
-    static JPanel wordPanel;
-    static WordManagerPanel wordManagerPanel;
-
+    JPanel pagePanel, wordPanel;
+    WordManagerPanel wordManagerPanel;
     public void launchPanel(){
         this.setLayout(null);
         this.setBounds(0,0,FRAME_WIDTH,FRAME_HEIGHT);
         this.setVisible(false);
-        pagePanel = new JPanel();
+        pagePanel = new ImagePanel(new ImageIcon("picture\\背景图.jpg").getImage());
         pagePanel.setBounds(0, 0, 170, FRAME_HEIGHT);
-        pagePanel.setBackground(Color.PINK);
         pagePanel.setLayout(null);
         back = new JButton("返回");
+        back.setIcon(SwingUtil.createAutoAdjustIcon(new ImageIcon("picture\\返回箭头.png").getImage(), true));
+        back.setBorderPainted(true);
+        back.setContentAreaFilled(false);
         back.setBounds(35, 40, 100, 100);
         back.addActionListener(e -> {
             Frame.starPage.setVisible(false);
             Frame.homePage.setVisible(true);
         });
         information = new JButton("资料");
+        information.setIcon(SwingUtil.createAutoAdjustIcon(new ImageIcon("picture\\单词列表.png").getImage(), true));
+        information.setBorderPainted(true);
+        information.setContentAreaFilled(false);
         information.setBounds(35, 220, 100, 100);
         information.addActionListener(e -> {
             Frame.starPage.setVisible(false);
             Frame.settingPage.setVisible(true);
         });
         star = new JButton("收藏单词");
+        star.setIcon(SwingUtil.createAutoAdjustIcon(new ImageIcon("picture\\单词收藏.png").getImage(), true));
+        star.setBorderPainted(false);
+        star.setContentAreaFilled(false);
         star.setBounds(35, 400, 100, 100);
 
         pagePanel.add(back);
         pagePanel.add(information);
         pagePanel.add(star);
         this.add(pagePanel);
-        wordPanel = new JPanel();
+
+
+        wordPanel = new ImagePanel(new ImageIcon("picture\\淡粉.jpeg").getImage());
         wordPanel.setBounds(195, 20, 570, FRAME_HEIGHT-80);
-        wordPanel.setBackground(Color.PINK);
         //bookPanel.setLayout(new FlowLayout()); // 使用FlowLayout作为示例，可以根据需要更改布局管理器
         wordPanel.setLayout(null);
         wordManagerPanel = new WordManagerPanel();
         wordManagerPanel.setBounds(20, 40, 520, FRAME_HEIGHT-150);
         wordPanel.add(wordManagerPanel);
-        Frame.starPage.add(wordPanel);
+        this.add(wordPanel);
+
     }
 
 }
 class WordManagerPanel extends JPanel {
     JTable table;
-    static DefaultTableModel model;
+    DefaultTableModel model;
     CheckBoxRenderer checkBoxRenderer;
     CheckBoxEditor checkBoxEditor;
+    static Color sPink = new Color(252,232,230);
+    static Color sWhite = new Color(254,243,235);
 
-    static int starPtr = 0;
-
-    static ArrayList<String[]> starList = new ArrayList<>();
+    //添加美工
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        ImageIcon img = new ImageIcon("picture\\淡粉.jpeg");
+        img.paintIcon(this, g, 0, 0);
+    }
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        int fieldX = 0;
+        int fieldY = 0;
+        int fieldWeight = getSize().width;
+        int fieldHeight = getSize().height;
+        Graphics2D g2d=(Graphics2D)g;
+        Color bg = new Color(128, 128, 128, 50);
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g.setColor(bg);
+        g.fillRoundRect(fieldX, fieldY, fieldWeight, fieldHeight, 20, 20);
+        super.paintChildren(g);//可以正常显示该组件中添加的组件
+    }
 
     public WordManagerPanel() {
-        this.setBackground(Color.pink);
         // 初始化表格模型
-        String[] columnNames = {"选择", "单词", "读音", "释义"};
+        String[] columnNames = {"选择", "单词", "意义", "词书"};
         model = new DefaultTableModel(columnNames, 0) {
             @Override
             public Class<?> getColumnClass(int columnIndex) {
@@ -451,19 +573,17 @@ class WordManagerPanel extends JPanel {
                 return super.getCellRenderer(row, column);
             }
         };
-        table.setBackground(Color.pink);
-        try (BufferedReader reader = new BufferedReader(new FileReader("star.txt"))){
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(",");
-                starList.add(new String[]{parts[0], parts[1], parts[2]});
-            }
-        }
-        catch (IOException e) {
-            e.printStackTrace();  // 处理异常，打印错误轨迹
-        }
+        table.setBackground(sPink);
+
         // 添加数据到表格
-        updateStar();
+        for (int i = 0; i < 5; i++) {
+            Vector<Object> rowData = new Vector<>();
+            rowData.add(false); // 多选框状态，默认未选中
+            rowData.add("单词" + (i + 1));
+            rowData.add("意义" + (i + 1));
+            rowData.add("词书" + (i + 1)); // 添加词书数据
+            model.addRow(rowData);
+        }
 
         // 设置复选框列的宽度
         TableColumn checkBoxColumn = table.getColumnModel().getColumn(0);
@@ -479,7 +599,7 @@ class WordManagerPanel extends JPanel {
     static class CheckBoxRenderer extends JCheckBox implements TableCellRenderer {
         public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
             setSelected((Boolean) value);
-            setBackground(isSelected ? Color.white: Color.pink); // 根据是否选中设置不同的背景颜色
+            setBackground(isSelected ? Color.white: sPink); // 根据是否选中设置不同的背景颜色
             return this;
         }
     }
@@ -488,25 +608,12 @@ class WordManagerPanel extends JPanel {
     static class CheckBoxEditor extends DefaultCellEditor {
         public CheckBoxEditor() {
             super(new JCheckBox());
-            ((JCheckBox) editorComponent).setBackground(Color.pink);  // 设置编辑器的背景颜色
+            ((JCheckBox) editorComponent).setBackground(sWhite);  // 设置编辑器的背景颜色
         }
 
         @Override
         public Object getCellEditorValue() {
             return ((JCheckBox)editorComponent).isSelected();
-        }
-    }
-
-
-    public static void updateStar(){
-        while(starPtr<starList.size()){
-            Vector<Object> rowData = new Vector<>();
-            rowData.add(false); // 多选框状态，默认未选中
-            rowData.add(starList.get(starPtr)[0]);
-            rowData.add(starList.get(starPtr)[1]);
-            rowData.add(starList.get(starPtr)[2]); // 添加词书数据
-            model.addRow(rowData);
-            starPtr++;
         }
     }
 }
